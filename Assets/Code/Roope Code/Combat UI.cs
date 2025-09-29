ba87e1ff8d2f6e84c4ff1b8d107e4dd7ce4a399e
Rhulhu <roope.hiironniemi@tuni.fi>
2025-09-29 15:32:52 +0300

    Implement Healthbar effect

--- Assets/Code/Roope Code/Combat UI.cs
+++ Assets/Code/Roope Code/Combat UI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ namespace Crognard
     public class CombatUI : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _nameText;
-        [SerializeField] private RectTransform _healthFill;
+        [SerializeField] private RectTransform _healthFill, _fillPrint;
         [SerializeField] private TextMeshProUGUI _healthText;
 
         public void SetupInfo(Unit unit)
@@ -19,6 +20,18 @@ namespace Crognard
         {
             _healthFill.localScale = new Vector3(amount / (float)max, 1, 1);
             _healthText.text = amount.ToString();
+            StartCoroutine(FillEffect());
+        }
+
+        private IEnumerator FillEffect()
+        {
+            for (int i = 0; i < 1;)
+            {
+                _fillPrint.localScale = Vector3.Lerp(_fillPrint.localScale, _healthFill.localScale, 1.5f * Time.deltaTime);
+                yield return null;
+                if (_fillPrint.localScale == _healthFill.localScale)
+                { i = 1; }
+            }
         }
     }
 }
